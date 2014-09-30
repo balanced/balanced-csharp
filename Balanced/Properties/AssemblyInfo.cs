f7aac46a54fb0a0f9357d6d4e491a95d46ab1b81
Ben Mills <ben@unfiniti.com>
2014-09-30 11:08:50 -0600

    Add description to nuget spec

--- Balanced/Properties/AssemblyInfo.cs
+++ Balanced/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Balanced")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Official client library for Balanced Payments.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Balanced")]

8758a82e6d9f858473f95995c623fd31667c2eab
richie serna <richie@balancedpayments.com>
2014-11-14 13:34:38 -0800

    add card hold scenario

---
+++ scenarios/scenarios/card_hold_order/request.cs
@@ -0,0 +1,12 @@
+﻿using Balanced;
+using System.Collections.Generic;
+
+Balanced.Balanced.configure("{{ api_key }}");
+
+Card card = Card.Fetch("{{ card_href }}");
+Dictionary<string, object> holdPayload = new Dictionary<string, object>();
+holdPayload.Add("amount", {{ payload.amount }});
+holdPayload.Add("description", "{{ payload.description }}");
+holdPayload.Add("order", "{{ order_href }}");
+CardHold cardHold = card.Hold(holdPayload);
+cardHold.Save();

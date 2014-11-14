f55a1fe316be94b8814aff7035ebc735dad460cf
richie serna <richie@balancedpayments.com>
2014-11-14 13:19:00 -0800

    Add scenarios for orders

---
+++ scenarios/scenarios/bank_account_debit_order/request.cs
@@ -0,0 +1,10 @@
+﻿using Balanced;
+using System.Collections.Generic;
+
+Balanced.Balanced.configure("{{ api_key }}");
+
+Order order = Order.Fetch("{{ order_href }}");
+BankAccount bankAccount = Card.Fetch("{{ bank_account_href }}");
+Dictionary<string, object> debitPayload = new Dictionary<string, object>();
+debitPayload.Add("amount", {{payload.amount}});
+Debit debit = order.DebitFrom(bankAccount, debitPayload);
---
+++ scenarios/scenarios/card_credit_order/request.cs
@@ -0,0 +1,10 @@
+﻿using Balanced;
+using System.Collections.Generic;
+
+Balanced.Balanced.configure("{{ api_key }}");
+
+Order order = Order.Fetch("{{ order_href }}");
+Card card = Card.Fetch("{{ card_href }}");
+Dictionary<string, object> debitPayload = new Dictionary<string, object>();
+debitPayload.Add("amount", {{payload.amount}});
+Debit debit = order.CreditTo(card, debitPayload);

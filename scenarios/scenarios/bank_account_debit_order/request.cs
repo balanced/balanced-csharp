3fdd7709f5420a5a558aadac98e15a7dd3fcdb9b
Richie Serna <rserna2010@gmail.com>
2014-11-14 14:02:16 -0800

    Update request.cs

--- scenarios/scenarios/bank_account_debit_order/request.cs
+++ scenarios/scenarios/bank_account_debit_order/request.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 Balanced.Balanced.configure("{{ api_key }}");
 
 Order order = Order.Fetch("{{ order_href }}");
-BankAccount bankAccount = Card.Fetch("{{ bank_account_href }}");
+BankAccount bankAccount = BankAccount.Fetch("{{ bank_account_href }}");
 Dictionary<string, object> debitPayload = new Dictionary<string, object>();
 debitPayload.Add("amount", {{payload.amount}});
 Debit debit = order.DebitFrom(bankAccount, debitPayload);

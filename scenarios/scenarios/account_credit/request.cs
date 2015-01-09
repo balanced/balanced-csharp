4924fae7b93a9820062cbbbd0b27bc1d29284684
richie serna <richie@balancedpayments.com>
2015-01-08 18:58:42 -0800

    add quotes to scenarios

--- scenarios/scenarios/account_credit/request.cs
+++ scenarios/scenarios/account_credit/request.cs
@@ -7,7 +7,7 @@ Account account = Account.Fetch("{{ href }}");
 
 Dictionary<string, object> creditPayload = new Dictionary<string, object>();
 creditPayload.Add("amount", {{payload.amount }} );
-creditPayload.Add("appears_on_statement_as", {{payload.appears_on_statement_as }} );
-creditPayload.Add("description", {{payload.description }} );
-creditPayload.Add("order", {{payload.order }} );
+creditPayload.Add("appears_on_statement_as", "{{payload.appears_on_statement_as }}" );
+creditPayload.Add("description", "{{payload.description }}" );
+creditPayload.Add("order", "{{payload.order }}" );
 Credit credit = account.Credit(creditPayload);
--- scenarios/scenarios/settlement_create/request.cs
+++ scenarios/scenarios/settlement_create/request.cs
@@ -5,7 +5,7 @@ Balanced.Balanced.configure("{{ api_key }}");
 
 Account account = Account.Fetch("{{ href }}");
 Dictionary<string, object> settlementPayload = new Dictionary<string, object>();
-settlementPayload.Add("funding_instrument", {{payload.funding_instrument }} );
-settlementPayload.Add("appears_on_statement_as", {{payload.appears_on_statement_as }} );
-settlementPayload.Add("description", {{payload.description }} );
+settlementPayload.Add("funding_instrument", "{{payload.funding_instrument }}" );
+settlementPayload.Add("appears_on_statement_as", "{{payload.appears_on_statement_as }}" );
+settlementPayload.Add("description", "{{payload.description }}" );
 Settlement settlements = account.Settle(settlementPayload);
